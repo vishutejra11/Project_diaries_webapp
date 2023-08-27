a5f7d19ee89e2703717139fa74971da2f466e083
Anil Kumar Patidar <98152428+vishutejra11@users.noreply.github.com>
2023-08-27 19:31:42 +0530

    final changes 2

--- frontend/src/component/AdminSidebar.jsx
+++ frontend/src/component/AdminSidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = (props) => {
     return (
          <div class="col-md-3 col-lg-2 sidebar-offcanvas pl-0" id="sidebar" role="navigation" style={{backgroundColor:"#435971"}}>
             <ul class="nav flex-column sticky-top pl-0 pt-5 p-3 mt-3 ">
-                <li class="nav-item mb-2 mt-3"><a class="nav-link text-light" href="#" ><h6>Jaydeep Patidar</h6></a></li>
+                <li class="nav-item mb-2 mt-3"><a class="nav-link text-light" href="#" ><h6>Anil Kumar Patidar</h6></a></li>
                 <li class="nav-item mb-2 "><a class="nav-link text-light" href="#"><i class="fas fa-user font-weight-bold"></i> <span className="ml-3">Overview</span></a></li>
                 
                 <li class="nav-item mb-2" ><a class="nav-link text-light" href="#"><i class="far fa-chart-bar font-weight-bold"></i> <span className="ml-3" onClick={()=>{fun("Pending")}}>Pending Posts</span></a></li>
